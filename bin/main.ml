7d2d52c4c48e9ae9543d2639dad959e187779b97
Alberto Garcia lopez <albgarci@c1r1s2.42madrid.com>
2024-05-26 20:33:54 +0200

    nothing special, trying to create a window

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
-let () = 
+let other () = 
   print_endline Lib.B.greet;
 
   let file = "moves" in
@@ -6,4 +6,14 @@ let () =
   let fd = open_in file in
     let line = Lib.Readfile.get_next_line fd in 
       List.iter (Printf.printf "%s") line;
-      flush stdout;
+      flush stdout
+
+let window () = 
+  try
+    Sdl.init[`VIDEO];
+    ignore (Sdl.Render.create_window_and_renderer ~width:0 ~height:0 ~flags:[Sdlwindow.Borderless]);
+    at_exit Sdl.quit
+  with
+   | e -> raise e
+
+let () = window ()

90ac404cd226939157c6c42f1aed347031363e3c
Alberto Garcia lopez <albgarci@c1r1s4.42madrid.com>
2024-05-31 19:31:54 +0200

    ocamlsdl1.2 again

--- bin/main.ml
+++ bin/main.ml
@@ -1,38 +1,16 @@
-(* https://github.dev/dbuenzli/tsdl/blob/master/test/fmts.ml *)
-module Sdl = Tsdl.Sdl
+let printf = Format.printf
 
+let rec poll_keys () =
+  let open Sdlevent in
+  if has_event () then begin
+    match wait_event () with
+    | KEYDOWN {keysym=key} -> printf "keydown '%c'@." (Sdlkey.char_of_key key)
+    | KEYUP {keysym=key} -> printf "keyup '%c'@." (Sdlkey.char_of_key key)
+    | _ -> Sdltimer.delay 5
+  end;
+  poll_keys ()
 
-(*
-let rec loop() = match Sdl.poll_event with
-  | Sdl.get_key -> Printf.printf "eo"; loop();
-  Some "a"
-  *)
-(*.create () in
-  let rec loop() = match Sdl.wait_event (Some e) with
-  | Error (`Msg e) -> Printf.printf "Not waiting for event %s" e; ()
-  | Ok () ->
-    match Sdl.Event.(get e keyboard_state) with
-    | _ -> Printf.printf Sdl.get_scancode_name e; loop()
-in
-Sdl.start_text_input();
-*)
-
-
-
-let main () = 
-  let inits = Sdl.Init.(video + events) in
-  match Sdl.init inits with
-  | Error (`Msg e) -> Tsdl.Sdl.log "Error: %s" e; exit 1
-  | Ok () -> 
-    match Sdl.create_window ~w:800 ~h:600 "Hello" Tsdl.Sdl.Window.input_focus with
-    | Error (`Msg e) -> Tsdl.Sdl.log "Create window error: %s" e; exit 1
-    | Ok window ->
-
-    
-      Sdl.pump_events ();
-      Sdl.delay 3000l;
-      Sdl.destroy_window window;
-      Sdl.quit();
-      exit 0
-
-let () = main()
+let () =
+  Sdl.init [`VIDEO];
+  (* Sdlkey.enable_key_repeat (); *)
+  poll_keys ()

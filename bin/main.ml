97717d5db10bdafa84057d30700cae4520ac6bc6
Alberto García <yeta1990@gmail.com>
2024-06-02 20:50:24 +0200

    grammar file to list

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,36 @@
 (* Importa la biblioteca TSDL *)
 open Tsdl
 
+let grammar_file_to_list file_name = 
+  let ic = open_in file_name in
+  let rec read_lines() =
+    try
+      let line = input_line ic in 
+      line :: read_lines() 
+    with End_of_file -> 
+      close_in ic;
+      []
+    in 
+    read_lines() 
+  
+
+let rec last = function 
+  | [] -> [] 
+  | [ x ] -> [x]
+  | _ :: t -> last t;;
+
 let print_pressed_keys lst = List.iter print_string lst; print_newline (); flush stdout
 
-let match_combo pressed_keys = match pressed_keys with
+let rec match_combo pressed_keys = match pressed_keys with
 | ["A"] -> print_pressed_keys pressed_keys; pressed_keys
 | ["A"; "B"] -> print_pressed_keys pressed_keys; pressed_keys
-| _ -> print_string "Nothing to do with keys "; print_pressed_keys pressed_keys; []
+| _ -> print_string "Nothing to do with keys "; print_pressed_keys pressed_keys; if List.length pressed_keys > 1 then match_combo (last pressed_keys) else []
 
 (* Función principal *)
 let () =
-  (* Inicializa SDL *)
+  
+  let grammar = grammar_file_to_list "moves" in 
+    List.iter print_string grammar; flush stdout;
   match Sdl.init Sdl.Init.video with
   | Error (`Msg e) -> Sdl.log "No se pudo inicializar SDL: %s" e; exit 1
   | Ok () -> ();
@@ -21,7 +41,6 @@ let () =
     | Error (`Msg e) -> Sdl.log "No se pudo crear la ventana: %s" e; exit 1
     | Ok win -> win
   in
-
   let rec event_loop (pressed_keys) =
     let event = Sdl.Event.create () in
     match Sdl.wait_event (Some event) with

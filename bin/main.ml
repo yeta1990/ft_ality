e820c100e20135639dc8a214ec6f95856ea7a8b7
Alberto García <yeta1990@gmail.com>
2024-06-03 00:28:25 +0200

    feature: checking pressed key against combo list

--- bin/main.ml
+++ bin/main.ml
@@ -38,46 +38,81 @@ let grammar_file_to_list file_name =
     in 
     read_lines()
   
+    (*
 let rec last = function 
   | [] -> [] 
   | [ x ] -> [x]
   | _ :: t -> last t;;
-
+*)
 let print_pressed_keys lst = 
     List.iter print_string lst; print_newline (); flush stdout
 
-let rec match_combo pressed_keys = match pressed_keys with
+let rec find_key grammar current_state last_key = 
+  match grammar with
+  | (key, description) :: tail -> 
+      if key = (current_state @ [last_key]) then (print_string description; flush stdout;) else find_key tail current_state last_key
+  | _ -> () 
+
+  
+let rec is_prefix prefix lst =
+  match prefix, lst with
+  | [], _ -> true
+  | _, [] -> false
+  | x::xs, y::ys -> x = y && is_prefix xs ys
+
+let is_final_state current_state possible_states =
+  let rec aux current_state possible_states =
+    match possible_states with
+    | [] -> true
+    | (state, _) :: tail ->
+        if is_prefix current_state state && List.length current_state < List.length state then
+          false
+        else
+          aux current_state tail
+  in
+  aux current_state possible_states
+(*
+let rec match_combo grammar pressed_keys = match pressed_keys with 
 | ["A"] -> print_pressed_keys pressed_keys; pressed_keys
 | ["A"; "B"] -> print_pressed_keys pressed_keys; pressed_keys
-| _ -> print_string "Nothing to do with keys "; print_pressed_keys pressed_keys; if List.length pressed_keys > 1 then match_combo (last pressed_keys) else []
-
+| _ -> print_string "Nothing to do with keys "; print_pressed_keys pressed_keys; if List.length pressed_keys > 1 then match_combo (grammar pressed_keys) else []
+*)
 let () =
   
-  let grammar = grammar_file_to_list "moves" in List.iter print_key_and_description grammar;
+
   match Sdl.init Sdl.Init.video with
   | Error (`Msg e) -> Sdl.log "No se pudo inicializar SDL: %s" e; exit 1
   | Ok () -> ();
 
-  let window =
-    match Sdl.create_window ~w:640 ~h:480 "Ventana de ejemplo" Sdl.Window.shown with
-    | Error (`Msg e) -> Sdl.log "No se pudo crear la ventana: %s" e; exit 1
-    | Ok win -> win
+  let grammar = grammar_file_to_list "moves"
   in
-  let rec event_loop (pressed_keys) =
-    let event = Sdl.Event.create () in
-    match Sdl.wait_event (Some event) with
-    | Error (`Msg e) -> Sdl.log "Error esperando un evento: %s" e; Sdl.destroy_window window; Sdl.quit ()
-    | Ok () ->
-      let event_type = Sdl.Event.get event Sdl.Event.typ in
-      match Sdl.Event.enum event_type with
-      | `Quit -> Sdl.log "Evento de salida recibido, cerrando."; Sdl.destroy_window window; Sdl.quit ()
-      | `Key_down -> 
-        let keycode = Sdl.Event.get event Sdl.Event.keyboard_keycode in
-        let keyname = Sdl.get_key_name keycode in
-        if keyname = "Escape" then (Sdl.destroy_window window; Sdl.quit(); exit 0); (* remove before the evaluation*)
-        let pressed_keys2 = match_combo (pressed_keys @ [keyname]) in 
-          event_loop (pressed_keys2)
-      | _ -> event_loop (pressed_keys)  (* Para otros eventos, continúa el bucle *)
-  in
-  event_loop ([]);
+    List.iter print_key_and_description grammar;
+    let window =
+      match Sdl.create_window ~w:640 ~h:480 "Ventana de ejemplo" Sdl.Window.shown with
+      | Error (`Msg e) -> Sdl.log "No se pudo crear la ventana: %s" e; exit 1
+      | Ok win -> win
+    in
+
+    let rec event_loop (pressed_keys) =
+      let event = Sdl.Event.create () in
+      match Sdl.wait_event (Some event) with
+      | Error (`Msg e) -> Sdl.log "Error esperando un evento: %s" e; Sdl.destroy_window window; Sdl.quit ()
+      | Ok () ->
+        let event_type = Sdl.Event.get event Sdl.Event.typ in
+        match Sdl.Event.enum event_type with
+        | `Quit -> Sdl.log "Evento de salida recibido, cerrando."; Sdl.destroy_window window; Sdl.quit ()
+        | `Key_down -> 
+          let keycode = Sdl.Event.get event Sdl.Event.keyboard_keycode in
+          let keyname = Sdl.get_key_name keycode in
+          if keyname = "Escape" then (Sdl.destroy_window window; Sdl.quit(); exit 0); (* remove before the evaluation*)
+
+          find_key grammar [] keyname;
+          if List.length pressed_keys > 0 then find_key grammar pressed_keys keyname;
+          if is_final_state (pressed_keys @ [keyname]) grammar = true then 
+            event_loop ([])
+          else
+            event_loop(pressed_keys @ [keyname])
+        | _ -> event_loop (pressed_keys)  
+    in
+    event_loop ([]);
   Sdl.quit ()

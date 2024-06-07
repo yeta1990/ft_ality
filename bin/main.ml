68632526f49e3eaf4bec98e4be96b40592e4d06b
Alberto García <yeta1990@gmail.com>
2024-06-08 00:59:54 +0200

    feature: showing key description when in an intermediate state

--- bin/main.ml
+++ bin/main.ml
@@ -70,7 +70,7 @@ let rec is_sublist sub lst =
  | _, [] -> false
  | x::xs, y::ys -> 
     (x = y && is_sublist xs ys) || is_sublist sub ys
-  
+
 
 (*
 avoids defining a state that is at the same time a substate/subset of another state. i.e:
@@ -82,7 +82,7 @@ are incompatible
 let rec validate_substates states =
   match states with
   | [] -> true
-  | ([_], _)::rest -> validate_substates rest
+  | ([_], _)::rest -> validate_substates rest (*not checking single keys*)
   | (s1, _)::rest ->
     let has_sublist =
       List.exists (fun (s2, _) -> 
@@ -94,17 +94,11 @@ let rec validate_substates states =
       in
         if has_sublist then false else validate_substates rest
 
-
-
-
 let update_state current_state pressed_key states =
   if key_exists (current_state @ [pressed_key]) states then
     current_state @ [pressed_key]
   else if key_exists current_state states then [pressed_key]
   else if List.length current_state = 0 then []
-  else if key_exists ([List.hd (List.rev current_state)] @ [pressed_key]) states then
-    [(List.hd (List.rev current_state))] @ [pressed_key]
-
   else
     [pressed_key]
 
@@ -146,14 +140,14 @@ let () =
         let event_type = Sdl.Event.get event Sdl.Event.typ in
         match Sdl.Event.enum event_type with
         | `Quit -> Sdl.log "Evento de salida recibido, cerrando."; Sdl.destroy_window window; Sdl.quit ()
-        | `Key_down -> 
+        | `Key_down ->
           let keycode = Sdl.Event.get event Sdl.Event.keyboard_keycode in
           let keyname = Sdl.get_key_name keycode in
           if keyname = "Escape" then (Sdl.destroy_window window; Sdl.quit(); exit 0); (* remove before the evaluation*)
           let current_state = update_state pressed_keys keyname grammar in 
             let (found, description) = get_description_by_key grammar current_state in  
               if found then (print_current_state current_state; print_string description; print_string "\n\n";flush stdout)
-              else if key_exists current_state grammar then print_current_state current_state
+              else if key_exists current_state grammar then (print_current_state current_state; let (_, description) = get_description_by_key grammar [keyname] in print_string description; print_string "\n\n")
               else print_string ("[" ^ keyname ^ "]: key not found, state reset\n"); flush stdout ;
               event_loop (current_state)
         | _ -> event_loop (pressed_keys)  
